ca4114e5c115592ffbb2903e659440c8ed3d8572
Kammula Keerthi <128952654+Keerthi29062005@users.noreply.github.com>
2024-06-05 12:11:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 import Images_front from './components/Total.jsx';
 import './App.css'; 
+import Board from './components/Board.jsx'
 
 function App() {
-  const [imagesVisible, setImagesVisible] = useState(false); 
+  const [imagesVisible, setImagesVisible] = useState(false);
+  const [frontGavulluCount, setFrontGavulluCount] = useState(0); // State to store countf
 
   const handleButtonClick = () => {
     setImagesVisible(!imagesVisible); 
   };
+
   const handleCounts = (countf, countb) => {
     console.log("after change!");
     console.log("Front Gavullu Count:", countf.countf);
     console.log("Back Gavullu Count:", countf.countb);
+    setFrontGavulluCount(countf.countf); // Update the countf state with a specific value
   }; 
+  
 
   return (
     <div className="container">
+     <Board frontGavulluCount={frontGavulluCount} />
       <h1 className='Heading_app'>PLAY</h1>
-      {imagesVisible && <Images_front onCounts= {handleCounts} />} {}
+      {imagesVisible && <Images_front onCounts={handleCounts} />}
       <button onClick={handleButtonClick} className='Apnabutton'>LET'S ROLL</button>
+      <h1>Front Gavullu Count: {frontGavulluCount}</h1> {/* Display the countf value */}
     </div>
   );
 }
