0e9df5d1504d15f8ccd14560bf3b4c8703760e81
Kammula Keerthi <128952654+Keerthi29062005@users.noreply.github.com>
2024-06-05 12:12:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+iimport React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Dup_Board from './components/Dup_Board.jsx'
 import './index.css';
 import './components/main.css';
 
-
 const RootComponent = () => (
   <div className="Entire">
-    <Dup_Board className="Box"/>
-    <App className="Box"/>
+    <App />
   </div>
 );
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
