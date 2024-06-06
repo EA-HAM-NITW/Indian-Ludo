688c7fea1ef9b8e82775927590af94553f0e5484
Heramb <hs22csb0f19@student.nitw.ac.in>
2024-06-06 12:02:08 +0530

    adding player numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
 import React, { useState } from 'react';
 import Images_front from './components/Total.jsx';
 import './App.css'; 
-import Board from './components/Board.jsx'
+import Board from './components/Board.jsx';
 
 function App() {
   const [imagesVisible, setImagesVisible] = useState(false);
-  const [frontGavulluCount, setFrontGavulluCount] = useState(0); // State to store countf
+  const [frontGavulluCount, setFrontGavulluCount] = useState(0);
+  const [playerCount, setPlayerCount] = useState(1);
+  const [isPlayerTurn, setIsPlayerTurn] = useState(true);
 
   const handleButtonClick = () => {
-    setImagesVisible(!imagesVisible); 
+    if (!imagesVisible) {
+      setPlayerCount(prevCount => prevCount + 1); 
+    }
+    setImagesVisible(prevVisible => !prevVisible);
+    setIsPlayerTurn(false); // After button click, it's not player's turn
   };
 
-  const handleCounts = (countf, countb) => {
+  const handleCounts = (counts) => {
     console.log("after change!");
-    console.log("Front Gavullu Count:", countf.countf);
-    console.log("Back Gavullu Count:", countf.countb);
-    setFrontGavulluCount(countf.countf); // Update the countf state with a specific value
+    console.log("Front Gavullu Count:", counts.countf);
+    console.log("Back Gavullu Count:", counts.countb);
+    setFrontGavulluCount(counts.countf);
   }; 
   
-
   return (
     <div className='Outer_container'>
       <div className='Inner_container1'>
-     <Board frontGavulluCount={frontGavulluCount} />
-     </div>
-     <div className='Inner_container2'>
-      {imagesVisible && <Images_front onCounts={handleCounts} />}
-      <button onClick={handleButtonClick} className='Apnabutton'>LET'S ROLL</button>
+        <Board frontGavulluCount={frontGavulluCount} />
+      </div>
+      <div className='Inner_container2'>
+        {imagesVisible && <Images_front onCounts={handleCounts} />}
+        <button onClick={handleButtonClick} className='Apnabutton'>LET'S ROLL</button>
+        <h2 className='player_name'>PLAYER {playerCount} HAS TO ROLL</h2>
       </div>
     </div>
   );
 }
 
 export default App;
+
