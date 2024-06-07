63051636d98403698f9f286662482131f9f41521
Kammula Keerthi <128952654+Keerthi29062005@users.noreply.github.com>
2024-06-07 18:23:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css';
 import './components/main.css';
-
+import Music from './components/Music.jsx'
 const RootComponent = () => (
   <div className="Entire">
     <App />
+      <Music/>
   </div>
 );
 
