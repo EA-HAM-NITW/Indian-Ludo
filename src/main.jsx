f78dede6153ec3d8b564e3ce7a9c2c223cc66105
Heramb <hs22csb0f19@student.nitw.ac.in>
2024-06-27 22:11:51 +0530

    created route

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css';
 import './components/main.css';
-import Music from './components/Music.jsx'
+import Home from './components/Home.jsx';
 
 const RootComponent = () => (
   <div className="Entire">
-    <App />
-    <Music/>
+    <Home/>
   </div>
 );
-
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
